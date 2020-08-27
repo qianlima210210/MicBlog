a55ae786e62082b9c9eaea8017897ea21c7a4cbb
maqianli <maqianli@nn.com>
2020-08-27 09:19:58 +0800

    no message

--- classes/View/Main/BaseViewController.swift
+++ classes/View/Main/BaseViewController.swift
@@ -7,18 +7,66 @@
 //
 
 import UIKit
+import SnapKit
 
 class BaseViewController: UIViewController {
+    
+    var statusBar_NavBar_View: UIView = UIView()
+    var baseContainerView: UIView = UIView()
     
     var isLogin = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initSbuViews()
+        
+        isLogin ? setupBaseContainerView() : setupVisitorView()
     }
     
+    func initSbuViews() -> Void {
+        //设置状态栏+导航栏
+        statusBar_NavBar_View.backgroundColor = .systemPink
+        view.addSubview(statusBar_NavBar_View)
+        var height = UIApplication.shared.statusBarFrame.height + (navigationController?.navigationBar.bounds.height ?? 0.0)
+        
+        statusBar_NavBar_View.snp.makeConstraints { (maker) in
+            maker.left.equalToSuperview()
+            maker.top.equalToSuperview()
+            maker.right.equalToSuperview()
+            maker.height.equalTo(height)
+        }
+        
+        //设置基本容器视图
+        baseContainerView.backgroundColor = .orange
+        view.addSubview(baseContainerView)
+        height = tabBarController?.tabBar.bounds.height ?? 0.0
+        
+        baseContainerView.snp.makeConstraints { (maker) in
+            maker.left.equalToSuperview()
+            maker.right.equalToSuperview()
+            maker.top.equalTo(statusBar_NavBar_View.snp.bottom).offset(0)
+            maker.bottom.equalTo(view.snp.bottom).offset(-height)
+        }
+    }
+    
+    func setupVisitorView() -> Void {
+        print("setupVisitorView")
+        let visitorView = Bundle.main.loadNibNamed("VisitorView", owner: nil, options: nil)?.first as? VisitorView
+        if let visitorView = visitorView {
+            visitorView.backgroundColor = .blue
+            baseContainerView.addSubview(visitorView)
+            
+            visitorView.snp.makeConstraints { (maker) in
+                maker.left.equalTo(baseContainerView).offset(20)
+                maker.right.equalTo(baseContainerView).offset(-20)
+                maker.height.equalTo(300)
+                maker.centerY.equalToSuperview()
+            }
+        }
+    }
 
-
+    func setupBaseContainerView() -> Void {
+        print("setupBaseContainerView")
+    }
 
 }
---
+++ classes/View/Main/VisitorView/VisitorView.swift
@@ -0,0 +1,49 @@
+//
+//  VisitorView.swift
+//  MicBlog
+//
+//  Created by maqianli on 2020/8/26.
+//  Copyright © 2020 nn. All rights reserved.
+//
+
+import UIKit
+
+class VisitorView: UIView {
+
+    //initWithFrame是UIView指定构造函数
+    //使用纯代码创建UIView对象时，调用此方法
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    //initWithCoder也是UIView指定构造函数
+    //使用sb或xib创建UIView时，调用此方法
+    required init?(coder: NSCoder) {
+        //fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
